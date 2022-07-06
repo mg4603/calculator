d4a1599e0eb396600f1821f55de6ee72b20e00d9
Michael George <michael.g.george@gmail.com>
2022-07-07 01:32:46 +0530

    Implement operate function and test all functions
    Implemented operate function that takes an operator and two numbers and
    then calls the right function on them.

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(number1, number2){
 function divide(number1, number2){
     return number1 / number2
 }
+
+function operate(operator, number1, number2){
+    if(operator === '+'){
+        return add(number1, number2);
+    }
+    if(operator === '-'){
+        return subtract(number1, number2);
+    }
+    if(operator === '*'){
+        return multiply(number1, number2);
+    }
+    if(operator == "/"){
+        return divide(number1, number2);
+    }
+}
+
+console.log(add(1, 2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
+console.log(operate('+', 1, 2));
+console.log(operate('-', 1, 2));
+console.log(operate('*', 1, 2));
+console.log(operate('/', 1, 2));
